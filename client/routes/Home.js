e5150881d6ffa34dd0425defb89e125e170febf5
chrisbodhi <cboette@gmail.com>
2015-07-05 21:53:01 +0000

    Add form for collecting poll questions. Add start of actions to handle submission of poll questions.

--- client/routes/Home.js
+++ client/routes/Home.js
@@ -1,10 +1,24 @@
 React = require('react');
 
 var Home = React.createClass({
+  getInitialState: function() {
+    return {value: "hola mundo"};
+  },
+  onSubmit: function() {
+    // save to db...
+  },
 	render: function() {
 		return (
       /* jshint ignore:start */
-			<p>Home page</p>
+			<div id="poll-form">
+        <form onsubmit="">
+          <label>Question for polling: </label>
+          <input type="text" id="question" placeholder="How much change is in my couch?" required autofocus />
+          <input type="text" name="response" placeholder="None." required />
+          <input type="text" name="response" placeholder="All of it." required />
+          <input type="submit" />
+        </form>
+      </div>
       /* jshint ignore:end */
 		);
 	}
