ea9166273f3863f3eb0894faf47ab49d7622d8ec
chrisbodhi <cboette@gmail.com>
2015-07-05 21:01:39 +0000

    Add Gruntfile with JS Hint and Express server and watch tasks.

---
+++ Gruntfile.js
@@ -0,0 +1,60 @@
+module.exports = function(grunt){
+  require('load-grunt-tasks')(grunt);
+  
+  grunt.initConfig({
+    
+    express: {
+      dev: {
+        options: {
+          port: 1337,
+          script: 'server.js'
+        }
+      }
+    },
+    
+    jshint: {
+      all: [
+        'client/**/*.js',
+        'Gruntfile.js', 
+        'models/*.js',  
+        'server.js'
+      ]
+    },
+    
+    watch: {
+      css: {
+        files: '**/*.less',
+        tasks: ['less'],
+        options: {
+          livereload: true
+        }
+      },
+      express: {
+        files: [
+          'client/**/*.js',
+          'Gruntfile.js', 
+          'models/*.js',  
+          'server.js'
+        ],
+        tasks: ['express:dev'],
+        options: {
+          spawn: false
+        }
+      },
+      js: {
+        files: [
+          'client/**/*.js',
+          'Gruntfile.js', 
+          'models/*.js',  
+          'server.js'
+        ],
+        options: {
+          livereload: true
+        },
+        tasks: ['jshint']
+      }
+    }
+  });
+  
+  grunt.registerTask('default', ['express:dev', 'watch']);
+};
